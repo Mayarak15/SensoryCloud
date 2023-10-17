c762ffe7a279ab44bbe16399f60fddcd8eb293a9
Mayarak15 <129475257+Mayarak15@users.noreply.github.com>
2023-10-17 16:48:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function search_animal() {
+    let input = document.getElementById('searchbar').value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    if (input === '') {
+        for (let i = 0; i < x.length; i++) { 
+            x[i].style.display = 'none';
+        }
+    } else {
+        for (let i = 0; i < x.length; i++) { 
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display = 'none';
+            } else {
+                x[i].style.display = 'list-item';                 
+            }
+        }
+    }
+}
+
+
+function MenuHove() {
+    menu = document.querySelector("#MenuEscondido")
+  
+  
+    menu.style.display = "block";
+  }
+  
+  function MenuHoveSaiu() {
+    menu = document.querySelector("#MenuEscondido")
+  
+    menu.style.display = "none";
+  }
+
+
+
+
+  
+  function contatosblack() {
+    
+    contatosdiv = document.querySelector("#divContatos");
+    contatosdiv.style.background = "#86969a";
+
+    setTimeout(
+        function (){
+            contatosdiv.style.background = "transparent";
+        }, 2000)
+   
+  }
